0f0e193bae96d28333fb5766b934cf129ee9022d
mannycolon <colonmanuel7@gmail.com>
2017-01-04 18:10:13 -0500

    removed all business logic from VerseDisplay.js pushing it up to container.js at the same time removing all state from the component

--- VerseDisplay.js
+++ VerseDisplay.js
@@ -8,59 +8,7 @@ const api = window.ModuleApi;
 const React = api.React;
 
 class VerseDisplay extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      chapter: null,
-      verse: null,
-    }
-    this.goToVerseListener = this.goToVerseListener.bind(this);
-  }
 
-  componentWillMount() {
-    //set the state of chapte and verse from the props
-    this.setState({
-      chapter:this.props.chapter,
-      verse: this.props.verse
-    })
-    //listen for 'goToVerse' event to update the current chapt and verse
-    api.registerEventListener('goToVerse', this.goToVerseListener);
-  }
-
-  componentWillUnmount() {
-    api.removeEventListener('goToVerse', this.goToVerseListener);
-  }
-  /**
-   * @description - This updates the state of the current chapter and verse so
-   * that the panes display the current verse based on an event.
-   * @param {Object} params - The current chapter and verse.
-   ******************************************************************************/
-  goToVerseListener(params) {
-    this.setState({
-      chapter: params.chapterNumber,
-      verse: params.verseNumber
-    });
-  }
-  /**
-   * @description - This handles the verse that will be display in the pane
-   * that the panes display the current verse based on an event.
-   * @return {String} content - The current verse.
-   ******************************************************************************/
-  displayVerse(){
-    var content = "";
-    if(this.props.input){
-      if(this.props.greek){
-        var greekVerse =  this.props.input[this.state.chapter][this.state.verse];
-        content = this.displayGreek(greekVerse);
-      }else{
-        var list = this.props.input;
-        content = list[this.state.chapter][this.state.verse];
-      }
-    }else {
-      console.warn("The prop input is undefined");
-    }
-    return content;
-  }
   /**
    * @description - This handles the verse that will be display in the pane in
    * the greek language. it generates Popover with Lexicon info about eahc word.
@@ -72,7 +20,8 @@ class VerseDisplay extends React.Component {
     return text.map((word) => {
       var PopoverTitle = <span>
                            {word.word + " | "}
-                           <a href={'http://studybible.info/mac/' + word.speech} target="_blank">
+                           <a href={'http://studybible.info/mac/' + word.speech}
+                              target="_blank">
                              <b>
                               {word.speech}
                              </b>
@@ -95,19 +44,22 @@ class VerseDisplay extends React.Component {
   }
 
   render() {
-    //this gets the book name which isnt being rendered currenlty
-    //TODO: find out if we want to render the book name to the panes since it
-    //wasnt requred based on the new mock ups.
-    var title = "";
-    if (this.props.input !== undefined) {
-      if (this.props.input.hasOwnProperty('title')) {
-        title = this.props.input.title;
+    let content = "";
+    if(this.props.input){
+      if(this.props.greek){
+        var greekVerse =  this.props.input[this.props.chapter][this.props.verse];
+        content = this.displayGreek(greekVerse);
+      }else{
+        var list = this.props.input;
+        content = list[this.props.chapter][this.props.verse];
       }
+    }else {
+      console.warn("The prop input is undefined");
     }
-
     return (
       <div>
-        <b>{this.state.verse + " "}</b>{this.displayVerse()}
+        <b>{this.props.verse + " "}</b>
+        {content}
       </div>
     );
   }
