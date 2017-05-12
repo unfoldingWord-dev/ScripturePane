e6bbcfebdbe991283ab7b21d1b16154b8f1bcaaa
Chris Whitfield <cdwhitfield73@gmail.com>
2017-05-12 16:05:28 -0700

    removed old code

--- css/Style.js
+++ css/Style.js
@@ -18,63 +18,8 @@ var style = {
       marginBottom: "-5px",
     }
   },
-  dropzone: {
-    active: {
-      border: '2px solid #727272',
-      backgroundColor: '#f5f5f5'
-    },
-    text: {
-      lineHeight: '200px',
-      verticalAlign: 'middle',
-      width: '100%'
-    },
-    main: {
-      width: '100%',
-      color: '#212121',
-      height: '200px',
-      border: '2px dashed #727272',
-      borderRadius: '5px',
-      fontSize: '25px'
-    }
-  },
-  menuItem: {
-    text: {
-      cursor: 'pointer'
-    },
-    flag: {
-      enabled: {
-        color: '#CC0000',
-        visibility: 'visible'
-      },
-      disabled: {
-        visibility: 'hidden'
-      }
-    },
-    statusIcon: {
-      correct: {
-        color: 'green',
-        display: 'initial'
-      },
-      replaced: {
-        color: 'gold',
-        display: 'initial'
-      },
-      flagged: {
-        color: 'red',
-        display: 'initial'
-      },
-      unchecked: {
-        display: 'none'
-      }
-    }
-  },
-  removePane:{
-    float: "right",
-    cursor: "pointer",
-    marginTop: "3px"
-  },
   headingDescription:{
-    color: "#747474",
+    color: "var(--text-color-light)",
     fontStyle: 'bold',
     fontFamily: "noto sans"
   },
