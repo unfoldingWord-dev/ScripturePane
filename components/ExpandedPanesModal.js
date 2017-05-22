2a726662d6749448281151659516dc5ff7a20070
Chris Whitfield <cdwhitfield73@gmail.com>
2017-05-22 14:11:55 -0700

    update close buttons

--- components/ExpandedPanesModal.js
+++ components/ExpandedPanesModal.js
@@ -3,7 +3,7 @@
   * resources button on the scripture pane module.
 ******************************************************************************/
 import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal, Glyphicon } from 'react-bootstrap';
 import ExpandedPane from './ExpandedPane';
 import AddBible from './AddBible';
 import style from '../css/Style';
@@ -40,19 +40,22 @@ class ExpandedPanesModal extends React.Component {
 
     return (
       <Modal show={show} onHide={onHide} bsSize="lg" aria-labelledby="contained-modal-title-sm">
-        <Modal.Header style={{ backgroundColor: "var(--accent-color-dark)" }} closeButton>
-          <Modal.Title id="contained-modal-title-sm"
-            style={{ textAlign: "center", color: "var(--reverse-color)" }}>
-
+        <Modal.Header style={{ backgroundColor: "var(--accent-color-dark)" }}>
+          <Modal.Title id="contained-modal-title-sm">
+              <Glyphicon
+                  onClick={onHide}
+                  glyph={"remove"}
+                  style={{color: "var(--reverse-color)", cursor: "pointer", fontSize: "18px", float: "right"}}
+              />
           </Modal.Title>
         </Modal.Header>
-        <Modal.Body style={{ padding: '0px', height: "550px", backgroundColor: "var(--reverse-color)", color: "var(--text-color)" }}>
-          <div style={{height: "550px", display: 'flex'}}>
+        <Modal.Body style={{ padding: '0px', height: "500px", backgroundColor: "var(--reverse-color)", color: "var(--text-color)" }}>
+          <div style={{height: "500px", display: 'flex'}}>
             {displayExpandedPanes}
           </div>
         </Modal.Body>
-        <Modal.Footer style={{ backgroundColor: "var(--reverse-color)" }}>
-          <Button bsStyle="prime" onClick={onHide}>Close</Button>
+        <Modal.Footer style={{ padding: '0', backgroundColor: "var(--reverse-color)" }}>
+          <button className="btn-prime" onClick={onHide}>Close</button>
         </Modal.Footer>
       </Modal>
     );
