fc325d870ac3744c90141534ff2797841812fc34
RoyalSix <jay.a.scottjr@biola.edu>
2017-06-28 12:53:53 -0700

    aded fixes

--- Container.js
+++ Container.js
@@ -11,88 +11,86 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 const NAMESPACE = "ScripturePane";
 
 class ScripturePane extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentPaneSettings: null,
-      modalVisibility: false,
-      expandedPaneVisibility: false,
-      staticPaneSettings: null,
-      selectedPane: false
-    };
-  }
-  /**
-   * @description - This removes a scripture source from the scripture pane.
-   * @param {number} key - position index of the scripture source language
-   *        in the currentPaneSettings array.
-   */
-  removePane(key) {
-    let { modulesSettingsReducer, actions } = this.props;
-    try {
-      let paneSettings = modulesSettingsReducer.ScripturePane.currentPaneSettings;
-    } catch (e) {
-      consle.warn(e);
+    constructor() {
+        super();
+        this.state = {
+            currentPaneSettings: null,
+            modalVisibility: false,
+            expandedPaneVisibility: false,
+            staticPaneSettings: null,
+            selectedPane: false
+        };
     }
-    paneSettings.splice(key, 1);
-    actions.setModuleSettings(NAMESPACE, 'currentPaneSettings', paneSettings);
-  }
-  /**
-  * @description this methos is called when an user selects a resource name to
-  * be added to the scripture pane and it sets the state with this name in the
-  * selectedPane property. it matches that name that was sleected with the
-  * staticPaneSettings.
-  * @param {object} event - An event object that we use to get the value
-  * selected by the user from the select element
-  * @return {state} This will set the state to the selectedPane object
-  */
-  selectSourceLanguage(event) {
-    let selectedBibleId = event.target.value;
-    this.setState({ selectedPane: selectedBibleId });
-  }
-  /**
-   * @description This handles loading a new resource to the scripture pane
-   * it gets the currentPaneSettings array (the info of all the panes currently
-   * rendered in the scripture pane component) from the checkstore. and then it
-   * adds the new pane information and saves it in the checkstore. Finally, it
-   * sets the modalVisibility to false to close the modal.
-   */
-  addPane() {
-    let { modulesSettingsReducer, actions } = this.props;
-    try {
-      let currentPaneSettings = modulesSettingsReducer[NAMESPACE].currentPaneSettings;
-      if (this.state.selectedPane) {
-        currentPaneSettings.push(this.state.selectedPane);
-        actions.setModuleSettings(NAMESPACE, 'currentPaneSettings', currentPaneSettings);
-        this.setState({ modalVisibility: false });
-      }
-    } catch (e) {
-      console.warn(e);
+    /**
+     * @description - This removes a scripture source from the scripture pane.
+     * @param {number} key - position index of the scripture source language
+     *        in the currentPaneSettings array.
+     */
+    removePane(key) {
+        let { settingsReducer, actions } = this.props;
+        if (settingsReducer.toolsSettings.ScripturePane) {
+            let paneSettings = settingsReducer.toolsSettings.ScripturePane.currentPaneSettings;
+            paneSettings.splice(key, 1);
+            actions.setToolSettings(NAMESPACE, 'currentPaneSettings', paneSettings);
+        }
     }
-  }
+    /**
+    * @description this methos is called when an user selects a resource name to
+    * be added to the scripture pane and it sets the state with this name in the
+    * selectedPane property. it matches that name that was sleected with the
+    * staticPaneSettings.
+    * @param {object} event - An event object that we use to get the value
+    * selected by the user from the select element
+    * @return {state} This will set the state to the selectedPane object
+    */
+    selectSourceLanguage(event) {
+        // let { settingsReducer } = this.props;
+        // let staticPaneSettings = settingsReducer.toolsSettings[NAMESPACE].staticPaneSettings;
+        let selectedBibleId = event.target.value;
+        this.setState({ selectedPane: selectedBibleId });
+    }
+    /**
+     * @description This handles loading a new resource to the scripture pane
+     * it gets the currentPaneSettings array (the info of all the panes currently
+     * rendered in the scripture pane component) from the checkstore. and then it
+     * adds the new pane information and saves it in the checkstore. Finally, it
+     * sets the modalVisibility to false to close the modal.
+     */
+    addPane() {
+        let { settingsReducer, actions } = this.props;
+        if (settingsReducer.toolsSettings[NAMESPACE]) {
+            let currentPaneSettings = settingsReducer.toolsSettings[NAMESPACE].currentPaneSettings;
+            if (this.state.selectedPane) {
+                currentPaneSettings.push(this.state.selectedPane);
+                actions.setToolSettings(NAMESPACE, 'currentPaneSettings', currentPaneSettings);
+                this.setState({ modalVisibility: false });
+            }
+        }
+    }
 
-  render() {
-    return (
-      <MuiThemeProvider>
-        <View
-          {...this.props}
-          contextId={this.props.contextIdReducer.contextId}
-          removePane={this.removePane.bind(this)}
-          modalVisibility={this.state.modalVisibility}
-          showModal={() => this.setState({ modalVisibility: true, selectedPane: false })}
-          hideModal={() => this.setState({ modalVisibility: false })}
-          expandedPaneVisibility={this.state.expandedPaneVisibility}
-          selectedPane={this.state.selectedPane}
-          showExpandModal={() => this.setState({ expandedPaneVisibility: true })}
-          hideExpandModal={() => this.setState({ expandedPaneVisibility: false })}
-          selectSourceLanguage={this.selectSourceLanguage.bind(this)}
-          addPane={this.addPane.bind(this)}
-        />
-      </MuiThemeProvider>
-    );
-  }
+    render() {
+        return (
+            <MuiThemeProvider>
+                <View
+                    {...this.props}
+                    contextId={this.props.contextIdReducer.contextId}
+                    removePane={this.removePane.bind(this)}
+                    modalVisibility={this.state.modalVisibility}
+                    showModal={() => this.setState({ modalVisibility: true, selectedPane: false })}
+                    hideModal={() => this.setState({ modalVisibility: false })}
+                    expandedPaneVisibility={this.state.expandedPaneVisibility}
+                    selectedPane={this.state.selectedPane}
+                    showExpandModal={() => this.setState({ expandedPaneVisibility: true })}
+                    hideExpandModal={() => this.setState({ expandedPaneVisibility: false })}
+                    selectSourceLanguage={this.selectSourceLanguage.bind(this)}
+                    addPane={this.addPane.bind(this)}
+                />
+            </MuiThemeProvider>
+        );
+    }
 }
 
 module.exports = {
-  name: NAMESPACE,
-  container: ScripturePane
+    name: NAMESPACE,
+    container: ScripturePane
 };
