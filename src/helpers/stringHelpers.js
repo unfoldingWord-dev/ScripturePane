424aed0ff4b18943fa2b7d24afb510ea839dfae7
mannycolon <colonmanuel7@gmail.com>
2018-03-08 01:50:47 -0500

    fixed multiple nested milestone bugs

--- src/helpers/stringHelpers.js
+++ src/helpers/stringHelpers.js
@@ -2,3 +2,9 @@
 export const isWord = (word => {
   return (typeof word !== 'string') && (word.word || (word.type === 'word'));
 });
+
+export const isNestedMilestone = word => {
+  let deepNestedChild = false;
+  if (word[0] && word[0][0]) deepNestedChild = word[0][0].type === 'word';
+  return Array.isArray(word) && word.length > 0 && word[0].type === 'word' || deepNestedChild;
+};
