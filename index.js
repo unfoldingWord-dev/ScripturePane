22f22a8b7013e118c773a19ef35ce14f1d96b768
Bruce McLean <mcleanb@photo-nomad.net>
2018-03-27 15:14:30 -0400

    Working on localization

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import Container from './src/Container';
+import {connectTool} from 'tc-tool';
+import path from 'path';
 
 const NAMESPACE = 'ScripturePane';
+const LOCALE_DIR = path.join(__dirname, './locale');
 
 export default {
   name: NAMESPACE,
-  container: Container
+  container: connectTool(LOCALE_DIR)(Container)
 };
